cf69f163eda49ddb1675255e0f33f1a09917c3ff
Kamaal Farah <kamaal.f1@gmail.com>
2024-10-14 12:56:25 +0000

    Supporting lazy dataframes

--- django_play/managers.py
+++ django_play/managers.py
@@ -14,14 +14,23 @@ PolarsModel = TypeVar("PolarsModel", bound=models.Model)
 
 
 class PolarsQuerySet(models.QuerySet):
-    def to_polars(self) -> pl.DataFrame:
+    def to_polars(self) -> pl.LazyFrame:
+        return self.to_eager_polars().lazy()
+
+    def to_eager_polars(self) -> pl.DataFrame:
         return pl.read_database(
             query=self.__parse_sql_statement(),
             connection=connection,
         )
 
-    def to_narwhals_from_polars(self) -> nw.DataFrame[pl.DataFrame]:
+    def to_narwhals_from_polars(self) -> nw.LazyFrame[pl.LazyFrame]:
         nw_data_frame = nw.from_native(self.to_polars())
+        assert isinstance(nw_data_frame, nw.LazyFrame)
+
+        return nw_data_frame
+
+    def to_narwhals_from_eager_polars(self) -> nw.DataFrame[pl.DataFrame]:
+        nw_data_frame = nw.from_native(self.to_eager_polars())
         assert isinstance(nw_data_frame, nw.DataFrame)
 
         return nw_data_frame
@@ -41,11 +50,17 @@ class PolarsQuerySet(models.QuerySet):
 
 
 class PolarsManager(models.Manager.from_queryset(PolarsQuerySet), Generic[PolarsModel]):
-    def to_polars(self) -> pl.DataFrame:
+    def to_polars(self) -> pl.LazyFrame:
         return self.all().to_polars()
 
-    def to_narwhals_from_polars(self) -> nw.DataFrame[pl.DataFrame]:
+    def to_eager_polars(self) -> pl.DataFrame:
+        return self.all().to_eager_polars()
+
+    def to_narwhals_from_polars(self) -> nw.LazyFrame[pl.LazyFrame]:
         return self.all().to_narwhals_from_polars()
+
+    def to_narwhals_from_eager_polars(self) -> nw.DataFrame[pl.DataFrame]:
+        return self.all().to_narwhals_from_eager_polars()
 
     def values(
         self, *fields: str | models.Combinable, **expressions: Any
